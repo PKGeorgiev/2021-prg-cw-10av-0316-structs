607dc07bbda1f1af890267a66950bd1fc68dd4b3
Ivan Dimov <iidimov18@codingburgas.bg>
2021-03-16 10:27:47 +0200

    Add STUDENT structure

---
+++ problems-iidimov18/problems-iidimov18.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<vector>
+#include <string>
+#include "Student.h"
+using namespace std;
+int main()
+{
+    STUDENT student = { 1,"Ivan","Dimov",17,10 };
+    std::cout << "Hello World!\n";
+}
