b68453c31d5a2ef0a4f15e20dfc0042c2d587659
ViktorVelizarov <58163160+ViktorVelizarov@users.noreply.github.com>
2021-03-16 10:28:23 +0200

    add STUDENT students

---
+++ main/student.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <vector>
+struct STUDENT
+{
+	size_t id;
+	std::string firstName;
+	std::string surname;
+	int age;
+	int grade;
+	std::string getFullName();
+};
