d99d2dfb57e891b4f8da8a8b33b13a8e67ee4250
yuki petrov <pmdyakov18@codingburgas.bg>
2021-03-16 10:23:24 +0200

    First Commit Struct STUDENT func getFullName

---
+++ problems-pmdyakov18/student.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <vector>
+
+struct STUDENT
+{
+	size_t id;
+	std::string firstName;
+	std::string surName;
+	int age;
+	int grade;
+	
+
+	std::string getFullName();
+};
