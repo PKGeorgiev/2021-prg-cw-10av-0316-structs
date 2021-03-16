df9773db7f57e957098f5c3654f83fd1863a0a72
Ivelin Bachvarov <ipbachvarov18@codingburgas.bg>
2021-03-16 10:24:24 +0200

    Add classwork

---
+++ problems-IPBachvarov18/problems-IPBachvarov18.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include<vector>
+#include<string>
+#include"student.h"
+using namespace std;
+
+int main()
+{
+	STUDENT student={1,"Ivelin","Bachvarov",17,12};
+
+	cout<<student.getFullName();
+}
+
